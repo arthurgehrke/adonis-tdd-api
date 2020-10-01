2433572ce3bdac7c5d00ca651fc915362e517223
arthurgehrke <arthurgehrke07@gmail.com>
2020-10-01 15:34:35 -0300

    test: ensure threadcontroller deletes a thread

--- test/functional/thread.spec.js
+++ test/functional/thread.spec.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { test, trait } = use("Test/Suite")("Thread");
+const Factory = use("Factory");
 
 trait("Test/ApiClient");
 
@@ -16,4 +17,12 @@ test("Should create a thread if correct data is provided", async ({
     .end();
 
   response.assertStatus(200);
+});
+
+test("should delete a thread", async ({ assert, client }) => {
+  const thread = await await Factory.model("App/Models/Thread").create();
+
+  const response = await client.delete(`threads/${thread.id}`).send().end();
+  console.log(response);
+  response.assertStatus(204);
 });
