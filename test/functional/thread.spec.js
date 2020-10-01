71fd7f8d94f13cd3c2894bcd601768753723b23c
arthurgehrke <arthurgehrke07@gmail.com>
2020-10-01 16:20:12 -0300

    test: ensure only authenticated users can use threads methods

--- test/functional/thread.spec.js
+++ test/functional/thread.spec.js
@@ -1,34 +1,58 @@
-"use strict";
+'use strict'
 
-const { test, trait } = use("Test/Suite")("Thread");
-const Factory = use("Factory");
+const { test, trait, beforeEach } = use('Test/Suite')('Thread')
+const Factory = use('Factory')
+const Thread = use('App/Models/Thread')
 
-trait("Test/ApiClient");
+trait('Test/ApiClient')
+trait('Auth/Client')
 
-test("Should create a thread if correct data is provided", async ({
-  client,
+beforeEach(async () => {
+  await Thread.query().delete()
+})
+
+test('Ensure authorized user can create a thread if correct data is provided', async ({
+  client
 }) => {
+  const user = await Factory.model('App/Models/User').create()
   const response = await client
-    .post("/threads")
+    .post('/threads')
+    .loginVia(user)
     .send({
-      title: "test title",
-      body: "body",
+      title: 'test title',
+      body: 'body'
     })
-    .end();
+    .end()
 
-  response.assertStatus(200);
-});
+  response.assertStatus(200)
 
-test("Should delete a thread", async ({ assert, client }) => {
-  const thread = await await Factory.model("App/Models/Thread").create();
+  const thread = await Thread.firstOrFail()
+  response.assertJSON({ thread: thread.toJSON() })
+})
 
-  const response = await client.delete(`threads/${thread.id}`).send().end();
+test('Ensure authorized users can delete a thread', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const thread = await Factory.model('App/Models/Thread').create()
+  const response = await client.delete(thread.url()).send().loginVia(user).end()
+  response.assertStatus(204)
 
-  response.assertStatus(204);
-});
+  assert.equal(await Thread.getCount(), 0)
+})
 
-test("Should have url property", async ({ assert }) => {
-  const thread = await Factory.model("App/Models/Thread").create();
+test('Should have url property', async ({ assert }) => {
+  const thread = await Factory.model('App/Models/Thread').create()
 
-  assert.equal(thread.url(), `threads/${thread.id}`);
-});
+  assert.equal(thread.url(), `threads/${thread.id}`)
+})
+
+test('Ensure unauthenticated users cannot create threads', async ({ client }) => {
+  const response = await client
+    .post('/threads')
+    .send({
+      title: 'test title',
+      body: 'body'
+    })
+    .end()
+
+  response.assertStatus(401)
+})
